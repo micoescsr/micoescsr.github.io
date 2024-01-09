3f6a43cf431e3898d80a6b0ec8d7298b7b22e4e5
micoescsr <efraimmicoescosura@gmail.com>
2024-01-10 00:57:37 +0800

    Initial Commit: Personal Website Setup

---
+++ script.js
@@ -0,0 +1,6 @@
+function openMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamb-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
